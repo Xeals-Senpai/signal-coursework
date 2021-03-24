826d322f9eca57aaca6a9549371e80b07096824b
Xeals <xxZeroxxism@yahoo.co.uk>
2021-03-24 12:55:02 +0000

    Implemented parent & child process and SIGTSP and SIGCHLD

--- OperatingSystemAndDriversCoursework.c
+++ OperatingSystemAndDriversCoursework.c
@@ -8,10 +8,13 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
 void parentsignal_handler(int);
+void childsignal_handler (int);
+void randomnumbergenerator ();
 
 void main() 
 {
@@ -20,31 +23,44 @@ void main()
 	const int MAXLINE=4096;
 	char line[MAXLINE];
 
-	pid = fork();
+	pid=fork();
 
 	if (pipe(fd) < 0) {
-		printf("Pipe Error!");
+	printf("Pipe error!");
 	} 
 
+	signal(SIGCHLD, parentsignal_handler);
+
 	if (pid < 0) {
-		printf("Fork Error!");
+		printf("Fork error!");
 	} else if (pid > 0) {
-		wait(NULL);
-		printf("This is the parent process\n");
-		printf("Hello! This is the parent process speaking\n");
+		printf("This is the parent process!\n");
+		printf("The process id is: %i\n", getpid());
 		pause();
 	} else {
-		printf("This is the Child process\n");
-		printf("Hello! This is the child process speaking\n");
+		signal(SIGTSTP, childsignal_handler);
+		printf("This is the child process!\n");
+		printf("The process id is: %i\n", getpid());
 	}
+
 }
 
-void parentsignal_handler(int sig) 
-{
+void parentsignal_handler(int signo) {
 	printf("The signal in the parent process has been captured\n");
 }
 
-void childsignal_handler(int sig) {
-	printf("The signal in the child process has been captured");
+void childsignal_handler(int signo) {
+	printf("\nThe signal in the child process has been captured\n");
+	//randomnumbergenerator();
 }
 
+void randomnumbergenerator() {
+	int number;
+	int number2 = 10;
+	printf("Welcome to random number generator!");
+	
+	if (number == number2) {
+		number = rand() % 40 + 10;
+		number2 = number;
+	} else printf("Random Number generated = %d\n", number);
+}
