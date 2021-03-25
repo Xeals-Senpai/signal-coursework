60ea39f737d1b67faa4bdce36bd97ca130b581b4
Xeals <xxZeroxxism@yahoo.co.uk>
2021-03-25 23:46:37 +0000

    Declared global variables and starting to implement the file descriptors and pipe()

--- OperatingSystemAndDriversCoursework.c
+++ OperatingSystemAndDriversCoursework.c
@@ -12,17 +12,19 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define  MAXLINE 4096 
+
 void parentsignal_handler(int);
 void childsignal_handler (int);
 void randomnumbergenerator ();
-int flag = 0;
+//void filecreator();
+void informationDisplay(); 
+
+const int line[MAXLINE];
 int pid;
+int fd[2];
 
 void main() {
-	int fd[2];
-	const int MAXLINE=4096;
-	char line[MAXLINE];
-
 	pid=fork();
 
 	if (pipe(fd) < 0) {
@@ -35,19 +37,17 @@ void main() {
 	} else if (pid == 0) {							//Child process		
 		signal(SIGTSTP, childsignal_handler);
 		signal(SIGINT, childsignal_handler);
-		printf("This is the Child process\n");
-		printf("Its process id is: %i\n", getpid());
 
 		while(sleep(5000)) {}
 	} else {
 		signal(SIGTSTP, SIG_IGN);
 		signal(SIGINT, SIG_IGN);
 		signal(SIGCHLD, parentsignal_handler);
-		printf("\nThis is the parent process");
-		printf("\nThe process id is: %i\n", getpid());			//Parent process
+		informationDisplay();
 		pause();
 	}
 }
+
 
 void parentsignal_handler(int signo) {						//Signal Handler for the parent process
 	switch (signo) {
@@ -79,4 +79,10 @@ void randomnumbergenerator() {
 	int number2 = 10;
 	printf("Welcome to the random number generator!");
 	printf("\nRandom number generated = %d\n", rand() % 40 + 10);
+}
+
+void informationDisplay() {
+	printf("Welcome to the information display\n");
+	printf("The parent's pid is: %i\n", getpid());
+	printf("The child's pid is: %i\n", pid);
 }
