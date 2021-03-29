f5868311eb132a16add96b73427ef1e51af5ab54
Xeals <xxZeroxxism@yahoo.co.uk>
2021-03-29 13:06:39 +0100

    Implemented a working pipes and redirection and file creation and configuration

--- OperatingSystemAndDriversCoursework.c
+++ OperatingSystemAndDriversCoursework.c
@@ -16,24 +16,20 @@
 #include <time.h>
 #include <string.h>
 
-#define  MAXLINE 4096 
-
 void parentsignal_handler(int);
 void childsignal_handler (int);
 void randomnumbergenerator ();
-void informationDisplay(); 
+void informationDisplay();
 
-const int line[MAXLINE];
-int pid,number,arr[10],fd[2];				//fd[0] is the file descriptor for the read end of the pipe whereas 
-											//fd[1] is the file descriptor for the write end of the pipe
+int pid,number,arr[10],fd[2];			//fd[0] is the file descriptor for the read end of the pipe
+										//fd[1] is the file descriptor for the write end of the pipe
 
 void main() {
-	int file = open("STATUS.txt", O_APPEND | O_CREAT, 0777);
 	pid = fork();
+	int file = open("STATUS.txt", O_TRUNC | O_CREAT | O_RDWR, 0777);
 	if (pipe(fd) < 0) {
 	printf("Pipe error!");
 	} 
-
 
 	if (pid < 0) {
 		printf("Fork error!");
@@ -41,37 +37,40 @@ void main() {
 		signal(SIGTSTP, childsignal_handler);		//setting signal handlers per signal for the child process
 		signal(SIGINT, childsignal_handler);
 
-				
-
+		close(fd[0]);
+		dup2(fd[1],1);
+		close(fd[1]);
 		while(sleep(5000)) {}
 	} else {										//parent process
 		signal(SIGTSTP, SIG_IGN);					//setting signal handlers per signal for the parent process
 		signal(SIGINT, SIG_IGN);
 		signal(SIGCHLD, parentsignal_handler);
 
-		write(fd[1], str, strlen(str));
-		printf("This is to be written to the file STATUS.txt"),
-		pause();
+		close(fd[1]);
+		dup2(file,STDOUT_FILENO);
+		close(file);
+		informationDisplay();
+		dup2(fd[0],0);
+		close(fd[0]);
+		pause();									//waits for the child process to finish
 	}
 }
 
-
 void parentsignal_handler(int signo) {						//Signal Handler for the parent process
 	switch (signo) {
-		case SIGCHLD: printf("\nChild process has been terminated!\n");
+		case SIGCHLD: printf("Child process has been terminated!\n");
 			      	  printf("Ending the whole process\n");
-				  	  informationDisplay();
 			          break;
 	}
 }
 
 void childsignal_handler(int signo) {						//Signal Handler for the child process
 	switch (signo) {
-		case SIGINT: printf("\nCtrl + c detected...\n");
+		case SIGINT: printf("Ctrl + c detected...\n");
 			         printf("Exiting the program!\n");
-			     	 exit(0);
+			     	 exit(1);
 			     	 break;
-		case SIGTSTP:printf("\nCtrl + z detected...\n");
+		case SIGTSTP:printf("Ctrl + z detected...\n");
 			     	 printf("Starting the random number generator....\n");
 			     	 randomnumbergenerator();
 			     	 break;
@@ -81,10 +80,9 @@ void childsignal_handler(int signo) {						//Signal Handler for the child proces
 void randomnumbergenerator() {							
 	//Random number generator function which runs everytime Ctrl+z is pressed
 	//and a random number between 10 and 50 is generated.
-	srand(time(NULL));
 	arr[number] = rand() % 40 + 10;
-	printf("Welcome to the random number generator!");
-	printf("\nRandom number generated = %d\n", arr[number]);
+	printf("Welcome to the random number generator!\n");
+	printf("Random number generated = %d\n", arr[number]);
 }
 
 void informationDisplay() {
@@ -92,3 +90,5 @@ void informationDisplay() {
 	printf("The parent's pid is: %i\n", getpid());
 	printf("The child's pid is: %i\n", pid);
 }
+
+
